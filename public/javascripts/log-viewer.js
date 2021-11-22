03cd5a103b39f869832ee840ad1268be78c005c4
Richard C <r.clifford@bham.ac.uk>
2021-11-22 14:33:42 +0000

    fixed bugs mod 3

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,10 +1,14 @@
 const connection = new WebSocket("ws://localhost:3000");
-const logWindow = document.querySelector("log-window");
+const logWindow = document.querySelector("#log-window");
 
-connection.onopen(() => {
-  connection.send("Hello from the client");
-});
+connection.onopen = () => {
+  connection.send("Hello from the client!");
+};
+
+connection.onmessage = (event) => {
+  logWindow.innerHTML = event.data;
+};
 
 connection.onmessage((event) => {
-  logWindow.innerHTML(event.data);
+  console.log("hello");
 });
